448ab8006f69a1d2f84150d8ce72fe948c998488
Yureva Lyubov <yureva.exceedteam@gmail.com>
2022-04-08 17:23:09 +0300

    Create updating tasks

--- src/component/taskTodo/index.jsx
+++ src/component/taskTodo/index.jsx
@@ -1,8 +1,13 @@
-import changeIcons from "../../icons/pen.svg";
-import deleteIcons from "../../icons/trash.svg";
+import {useState} from 'react';
+import changeIcons from '../../icons/pen.svg';
+import deleteIcons from '../../icons/trash.svg';
+import doneIcons from '../../icons/check.svg';
+import closeIcons from '../../icons/close.svg';
 import './task.scss';
 
 const Task = ({ task, onChangeTasks }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [value, setValue] = useState(task.text);
 
   const deleteTask = async () => {
     const answer = window.confirm('Are you sure?');
@@ -13,18 +18,76 @@ const Task = ({ task, onChangeTasks }) => {
     onChangeTasks();
   };
 
+  const updateTask = async () => {
+    await fetch('http://localhost:8000/updateTask', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(task)
+    });
+    onChangeTasks();
+  };
+
+  const onChangeCheckbox = () => {
+    task.isCheck = !task.isCheck;
+    setIsEdit(false);
+    updateTask();
+  };
+
+  const saveUpdateText = () => {
+    const text = value.trim();
+    if (!text) return alert('Enter text!');
+    task.text = text;
+    updateTask();
+    setValue(text);
+    setIsEdit(false);
+  };
+
   return (
     <div className='box'>
       <div className='task'>
-        <input value={task.isCheck} type='checkbox' />
-        <p>{task.text}</p>
+        <input
+          checked={task.isCheck}
+          type='checkbox'
+          onChange={() => onChangeCheckbox()}
+        />
+        <p
+          className={task.isCheck ? 'decoration' : isEdit ? 'displayNone' : ''}
+        >
+          {task.text}
+        </p>
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className={isEdit ? 'inputText' : 'displayNone'}
+        />
       </div>
       <div className='iconsBox'>
-        <img src={changeIcons} alt='changeIcon' />
+        <img
+          src={changeIcons}
+          alt='changeIcon'
+          className={task.isCheck || isEdit ? 'displayNone' : ''}
+          onClick={() => setIsEdit(!isEdit)}
+        />
         <img
           src={deleteIcons}
           alt='deleteIcon'
-          onClick={() => deleteTask()}
+          className={isEdit ? 'displayNone' : ''}
+          onClick={deleteTask}
+        />
+        <img
+          src={doneIcons}
+          alt='doneIcons'
+          className={isEdit ? '' : 'displayNone'}
+          onClick={saveUpdateText}
+        />
+        <img
+          src={closeIcons}
+          alt='closeIcons'
+          className={isEdit ? '' : 'displayNone'}
+          onClick={() => setIsEdit(!isEdit)}
         />
       </div>
     </div>
